731b93554296d1d313bec551412416b8a1f1fca4
yeangPan <yeangPan@outlook.com>
2023-02-28 00:18:43 +0800

    update cpp template

--- cppdev/main.cc
+++ cppdev/main.cc
@@ -1,1 +1,28 @@
+/***********************************************************************
+ * @file: main.cc
+ * @brief: header file
+ * @history:   
+ * @date:2023-02-28
+ * @version: v1.1 
+ * @author:  pyang
+ * @contact: yeangPan@outlook.com
+ * @license: Apache License Version 2.0, 2023 yangp
+ * @description:    
+ * 
+ * 
+ * 
+ * 
+ * @Copyright:  (C)  2023  .pyang. all right reserved
+***********************************************************************/
+
+#include <iostream>;
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
 
